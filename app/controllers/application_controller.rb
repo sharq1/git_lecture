1470e45e2afc63ddad7f3e1d684eae27f34d0609
Damian Duda <damian.duda.89@gmail.com>
2014-09-08 19:26:18 +0200

    Account expiration check;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :check_something
+  before_filter :check_account
 
-  def check_something
-    # TODO
-    binding.pry
+  def check_account
+    if current_user.account_expired?
+      SaletMailer.expired_account(current_user).deliver
+      sign_out
+    end
   end
 
 end
