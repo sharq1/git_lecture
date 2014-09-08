ee619a4d0ff38b9af60c4fcf3bf1da6bf961ca07
Damian Duda <damian.duda.89@gmail.com>
2014-09-08 17:02:06 +0200

    global before filter;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :check_something
+
+  def check_something
+    # TODO
+    binding.pry
+  end
 
 end
